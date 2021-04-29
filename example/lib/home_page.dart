82e7d4eae7950151f121269845077f3fc18d1566
imaNNeoFighT <iman.neofight@gmail.com>
2021-04-29 21:20:28 +0430

    Implement UI of all needed elements.

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -3,9 +3,22 @@ import 'package:foolakey/foolakey.dart';
 
 import 'widgets/service_status_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final _inAppBillingKey =
       'MIHNMA0GCSqGSIb3DQEBAQUAA4G7ADCBtwKBrwDbkRScfggn+JSs+DzcZK20ZbxKPKv060aekC4dxqapamlgf9PncC5/4sqhUU4SdeKE770H1s7dJhmV5QEnzLawJTgiTzD3RFcadl2H4dduro/KxVyAe5nNKE/Xg+uRalLU/Hw9Or44m2xDyWESWj8sqweaGDUnsoHWJFsyVwwIj15fx3cDX6kjObC0gYns1o79x+COWCqyIlDwE2Pf7Xum55FASKFH8lqlYpEzR38CAwEAAQ==';
+
+  bool customPurchases;
+
+  @override
+  void initState() {
+    customPurchases = false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +37,49 @@ class HomePage extends StatelessWidget {
           }
 
           return Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 18,
-                ),
-                ServiceStatusWidget(snapshot),
-              ],
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 24,
+                  ),
+                  ServiceStatusWidget(snapshot),
+                  Expanded(child: Container()),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Product id'),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        customPurchases = !customPurchases;
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                      child: Row(
+                        children: [
+                          Switch(
+                            value: customPurchases,
+                            onChanged: (newValue) {
+                              setState(() {
+                                customPurchases = newValue;
+                              });
+                            },
+                          ),
+                          Text('Consume Purchase')
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('PURCHASE')),
+                  ElevatedButton(onPressed: () {}, child: Text('SUBSCRIBE')),
+                  ElevatedButton(onPressed: () {}, child: Text('CHECK IF USER PURCHASED THIS ITEM')),
+                  ElevatedButton(onPressed: () {}, child: Text('CHECK IF USER SUBSCRIBED THIS ITEM')),
+                  Expanded(child: Container()),
+                ],
+              ),
             ),
           );
         },
